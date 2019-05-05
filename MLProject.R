9dab2371f85485a27d9e662743275efd4dc815cc
Tayyip MUSLU <muhammet.muslu@ceng.deu.edu.tr>
2019-05-05 09:50:05 +0300

    Update MLProject.R

--- MLProject.R
+++ MLProject.R
@@ -1,18 +1,17 @@
 #include data preparation file
-source(file = "Preparation.R")
+source(file = "Preparation.R") # just shows worked dataset after data preparation.
 
-count(is.na(mush))
 #show current dataset
-head(mush)
-summary(mush)
+head(dataset)
+summary(dataset)
 
 #begin the logistic function
 set.seed(579642)  #Set the seed for reproducibility
-train_index <- sample(1:nrow(mush), size=nrow(mush)*0.8) # randomly choice rows
-test  <- mush[-train_index,]
-train <- mush[train_index,]
+train_index <- sample(1:nrow(dataset), size=nrow(dataset)*0.8) # randomly choice rows
+test  <- dataset[-train_index,]
+train <- dataset[train_index,]
 
-try1 = glm(formula = class ~ ., data = mush, subset = train, family = "binomial")
+try1 = glm(formula = class ~ ., data = dataset, family = "binomial")
 summary(try1)
 display(try1, digits = 4)
 help(glm)
