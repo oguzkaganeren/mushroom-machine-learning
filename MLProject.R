6f3358ddb8b7a40c21365c4440ff0b5a54c99094
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2019-05-05 12:09:11 +0300

    add model fitting

--- MLProject.R
+++ MLProject.R
@@ -2,16 +2,44 @@
 source(file = "Preparation.R") # just shows worked dataset after data preparation.
 
 #show current dataset
-head(dataset)
-summary(dataset)
+head(mushroom)
+summary(mushroom)
 
-#begin the logistic function
+
+
+#begin the logistic function (Model fitting)
 set.seed(579642)  #Set the seed for reproducibility
-train_index <- sample(1:nrow(dataset), size=nrow(dataset)*0.8) # randomly choice rows
-test  <- dataset[-train_index,]
-train <- dataset[train_index,]
+train_index <- sample(1:nrow(mushroom), size=nrow(mushroom)*0.8) # randomly choice rows
+test  <- mushroom[-train_index,]
+train <- mushroom[train_index,]
 
-try1 = glm(formula = class ~ ., data = dataset, family = "binomial")
-summary(try1)
-display(try1, digits = 4)
-help(glm)
+library(arm)
+try1 = bayesglm(formula = class ~ ., data = train, family=binomial(link='logit'))
+
+glm.probs <- predict(try1, type = "response")
+glm.probs[1:5]
+glm.pred  <- ifelse(glm.probs > 0.5,"p","e")
+misClasificError <- mean(glm.pred != train$class)
+print(paste('Accuracy',1-misClasificError))
+
+#Assessing the predictive ability of the model
+fitted.results <- predict(try1,newdata=subset(test),type='response')
+fitted.results <- ifelse(fitted.results > 0.5,"p","e")
+
+misClasificError <- mean(fitted.results != test$class)
+print(paste('Accuracy',1-misClasificError))
+
+#ROC Curve
+#install.packages("ROCR")
+library(ROCR)
+p <- predict(try1, newdata=subset(test), type="response")
+pr <- prediction(p, test$class)
+prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+plot(prf)
+
+auc <- performance(pr, measure = "auc")
+auc <- auc@y.values[[1]]
+auc
+#https://www.r-bloggers.com/how-to-perform-a-logistic-regression-in-r/
+
+
--- Preparation.R
+++ Preparation.R
@@ -38,19 +38,40 @@ for (var in 1:ncol(mushroom)) {
 #after data preparation of missing values.
 #missmap(mushroom, main = "After data preparation of missing values")
 
+
 drops <- c("veil_type") #there is one unique values of veil_type, we can remove this column in our dataset.
 mushroom <- mushroom[ , !(names(mushroom) %in% drops)] #remove veil_type
 #head(mushroom) # without numeric values, pure non preparing
 
 ## categoric to numeric without target
-library(mlr)
-mush <- createDummyFeatures(mushroom[2:22])
-mush$class <- mushroom$class
+mushroom$cap_shape <- scale(as.numeric(mushroom$cap_shape), center = TRUE, scale = TRUE)
+mushroom$cap_surface <- scale(as.numeric(mushroom$cap_surface), center = TRUE, scale = TRUE)
+mushroom$cap_color <- scale(as.numeric(mushroom$cap_color), center = TRUE, scale = TRUE)
+mushroom$bruises <- scale(as.numeric(mushroom$bruises), center = TRUE, scale = TRUE)
+mushroom$odor <- scale(as.numeric(mushroom$odor), center = TRUE, scale = TRUE)
+mushroom$gill_attachement <-scale(as.numeric(mushroom$gill_attachement), center = TRUE, scale = TRUE)
+mushroom$gill_spacing <- scale(as.numeric(mushroom$gill_spacing), center = TRUE, scale = TRUE)
+mushroom$gill_size <- scale(as.numeric(mushroom$gill_size), center = TRUE, scale = TRUE)
+mushroom$gill_color <- scale(as.numeric(mushroom$gill_color), center = TRUE, scale = TRUE)
+mushroom$stalk_shape <- scale(as.numeric(mushroom$stalk_shape), center = TRUE, scale = TRUE)
+mushroom$stalk_root <- scale(as.numeric(mushroom$stalk_root), center = TRUE, scale = TRUE)
+mushroom$stalk_surface_above_ring <- scale(as.numeric(mushroom$stalk_surface_above_ring), center = TRUE, scale = TRUE)
+mushroom$stalk_surface_below_ring <- scale(as.numeric(mushroom$stalk_surface_below_ring), center = TRUE, scale = TRUE)
+mushroom$stalk_color_above_ring <- scale(as.numeric(mushroom$stalk_color_above_ring), center = TRUE, scale = TRUE)
+mushroom$stalk_color_below_ring <- scale(as.numeric(mushroom$stalk_color_below_ring), center = TRUE, scale = TRUE)
+mushroom$veil_color <- scale(as.numeric(mushroom$veil_color), center = TRUE, scale = TRUE)
+mushroom$ring_number <- scale(as.numeric(mushroom$ring_number), center = TRUE, scale = TRUE)
+mushroom$ring_type <- scale(as.numeric(mushroom$ring_type), center = TRUE, scale = TRUE)
+mushroom$spore_print_color <- scale(as.numeric(mushroom$spore_print_color), center = TRUE, scale = TRUE)
+mushroom$population <- scale(as.numeric(mushroom$population), center = TRUE, scale = TRUE)
+mushroom$habitat <- scale(as.numeric(mushroom$habitat), center = TRUE, scale = TRUE)
+
+
 #After the mode process, the graph
 
-head(mush)
+head(mushroom)
 
 #head(mushroom) # list the dataset after fill missing values, categoricial to numericial, and normalization.
 # remove unncessary data
-rm(columnNames,drops,var,Mode, mushroom)
+rm(columnNames,drops,var,Mode)
 
