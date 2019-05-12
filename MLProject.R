fb6417a91a6638932bd7e5a3d41e5b4f871fc7a0
Tayyip MUSLU <muhammet.muslu@ceng.deu.edu.tr>
2019-05-12 11:13:22 +0300

    last try to find warning of 0 and 1

--- MLProject.R
+++ MLProject.R
@@ -1,36 +1,57 @@
+#install.packages("Amelia")
+#install.packages("caret")
+library(robustHD) # standardize function
+library(arm)
+library(caret)
+library(purrr)
+library(Amelia)
 #include data preparation file
 source(file = "Preparation.R") # just shows worked dataset after data preparation.
 
+TRAIN_SIZE  = 0.8 
+#NUM_OF_FOLD = 10
+#TRAIN_DATA  = list()
+
 #show current dataset
-head(mushroom)
-summary(mushroom)
-
-
+#head(mushroom)
+#summary(mushroom)
 
 #begin the logistic function (Model fitting)
-set.seed(579642)  #Set the seed for reproducibility
-train_index <- sample(1:nrow(mushroom), size=nrow(mushroom)*0.8) # randomly choice rows
+set.seed(1579642)  #Set the seed for reproducibility
+
+train_index <- sample(1:nrow(mushroom), size=nrow(mushroom)*TRAIN_SIZE) # randomly choice rows
 test  <- mushroom[-train_index,]
 train <- mushroom[train_index,]
 
-library(arm)
-#if we calculate the linear regression model, remove family part
-try1 = bayesglm(formula = class ~ ., data = train, family=binomial(link='logit'))
-
-glm.probs <- predict(try1, type = "response")
-probs <- exp(glm.probs)/(1+exp(glm.probs)) #gives you probability that y=1 for each observation
-
-glm.probs[1:5]
-glm.pred  <- ifelse(glm.probs > 0.5,"p","e")
-misClasificError <- mean(glm.pred != train$class)
-print(paste('Accuracy',1-misClasificError))
+start_time <- Sys.time()
+model = glm(formula = class ~ ., data = train, family = binomial())
+#anova(model)
+glm.pred  <- ifelse(predict(model, type = "response") > 0.5,"p","e")
+train_err <- mean(glm.pred != train$class)
 
 #Assessing the predictive ability of the model
-fitted.results <- predict(try1,newdata=subset(test),type='response')
+fitted.results <- predict(model,newdata=subset(test),type='response')
 fitted.results <- ifelse(fitted.results > 0.5,"p","e")
+test_err <- mean(fitted.results != test$class)
 
-misClasificError <- mean(fitted.results != test$class)
-print(paste('Accuracy',1-misClasificError))
+print(paste('Train : Accuracy',1-train_err,'Test : Accuracy',1-test_err))
+end_time <- Sys.time()
+
+print(paste("running time => ", (end_time-start_time)))
+
+
+
+
+
+
+
+# crossvalidation ? does not work
+train_control = trainControl(method="repeatedcv", number=5, repeats=5)
+modelLog = train(formula = class ~ ., data=train, method="glm", family=binomial, trControl=train_control)
+PredTrain = predict(modelLog, newdata=train, type="raw") 
+table(train$class, PredTrain > 0.5)
+
+
 
 #ROC Curve
 #install.packages("ROCR")
--- Preparation.R
+++ Preparation.R
@@ -1,27 +1,4 @@
-#install.packages("Amelia")
-#library(Amelia)
-columnNames <- c(
-  "class", "cap_shape", "cap_surface", 
-  "cap_color", "bruises", "odor", 
-  "gill_attachement", "gill_spacing", "gill_size", 
-  "gill_color", "stalk_shape", "stalk_root", 
-  "stalk_surface_above_ring", "stalk_surface_below_ring", "stalk_color_above_ring", 
-  "stalk_color_below_ring", "veil_type", "veil_color", 
-  "ring_number", "ring_type", "spore_print_color", 
-  "population", "habitat")
-
-#include dataset from disk
-mushroom <- read.table("agaricus-lepiota.data",
-                       sep = ",",
-                       na.strings = "?",
-                       colClasses = NA,
-                       header = FALSE,
-                       col.names= columnNames
-)
-
-
-#missmap(mushroom, main = "Missing values vs observed")
-
+source(file = "LoadDataset.R") # load dataset
 #replace NA values to columns mode
 Mode <- function (x, na.rm) {
   xtab <- table(x)
@@ -34,46 +11,27 @@ for (var in 1:ncol(mushroom)) {
   mushroom[is.na(mushroom[,var]),var] <- Mode(mushroom[,var], na.rm = TRUE)
 }
 
-#Analysis
-
-library(ggplot2)
-library(gridExtra)
-myplots <- list()  # new empty list
-for (i in 1:ncol(mushroom)) {
-  p1 <- eval(substitute(
-    ggplot(aes(x = mushroom[[i]]), data = mushroom) +
-      geom_histogram(stat = "count") +
-      facet_wrap(~class) +
-      xlab(colnames(mushroom)[i]))
-    ,list(i = i))
-  myplots[[i]] <- p1  # add each plot into plot list
-}#my precious
-#we plot the histograms of each category and split them into two graphs according to their edibility.
-#grid.arrange(myplots[[2]],myplots[[3]],myplots[[4]],myplots[[5]], ncol = 2)
-#grid.arrange(myplots[[6]],myplots[[7]],myplots[[8]],myplots[[9]], ncol = 2)
-#grid.arrange(myplots[[10]],myplots[[11]],myplots[[12]],myplots[[13]], ncol = 2)
-#grid.arrange(myplots[[14]],myplots[[15]],myplots[[16]],myplots[[17]], ncol = 2)
-#grid.arrange(myplots[[18]],myplots[[19]],myplots[[20]],myplots[[21]],myplots[[22]],myplots[[23]], ncol = 2)
-
 #after data preparation of missing values.
 #missmap(mushroom, main = "After data preparation of missing values")
 
-
+#unique(mushroom$veil_type) # there is an unique value of veil_type column.
 drops <- c("veil_type") #there is one unique values of veil_type, we can remove this column in our dataset.
 mushroom <- mushroom[ , !(names(mushroom) %in% drops)] #remove veil_type
 #head(mushroom) # without numeric values, pure non preparing
 
 ## categoric to numeric without target
+
 for (var in 2:ncol(mushroom)) {
-  mushroom[[var]] <- as.numeric( mushroom[[var]])
+  mushroom[[var]] <- as.integer( mushroom[[var]])
 }
 
+#normalize integer values without class variable by robustHD library
+#mushroom[,2:22] <-  standardize(mushroom[,2:22])
 
-#After the mode process, the graph
-
-#head(mushroom)
+"for (var in 1:ncol(mushroom)) {
+  mushroom[[var]] <- as.factor( mushroom[[var]])
+}"
 
 #head(mushroom) # list the dataset after fill missing values, categoricial to numericial, and normalization.
 # remove unncessary data
 rm(columnNames,drops,var,Mode)
-
