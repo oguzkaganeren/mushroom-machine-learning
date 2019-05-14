997f3ea5e25a90b6a44d78f3488f957dc2c74d61
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2019-05-14 18:37:38 +0300

    rework

--- MLProject.R
+++ MLProject.R
@@ -1,6 +1,5 @@
 #install.packages("Amelia")
 #install.packages("caret")
-library(robustHD) # standardize function
 library(arm)
 library(caret)
 library(purrr)
@@ -17,53 +16,82 @@ TRAIN_SIZE  = 0.8
 #summary(mushroom)
 
 #begin the logistic function (Model fitting)
-set.seed(1579642)  #Set the seed for reproducibility
+set.seed(48425)  #Set the seed for reproducibility
 
 train_index <- sample(1:nrow(mushroom), size=nrow(mushroom)*TRAIN_SIZE) # randomly choice rows
 test  <- mushroom[-train_index,]
 train <- mushroom[train_index,]
 
-start_time <- Sys.time()
+#start_time <- Sys.time()
 model = glm(formula = class ~ ., data = train, family = binomial())
-#anova(model)
-glm.pred  <- ifelse(predict(model, type = "response") > 0.5,"p","e")
-train_err <- mean(glm.pred != train$class)
+summary(model)
+#NA değerler var, bunun sebebi birden fazla attribute un iyi bir şekilde eşleşmesi
+#bu durumu istemiyoruz o yüzden NA'lı attributeları kaldırıyoruz
+drops <- c("stalk_color_above_ring","stalk_color_below_ring","veil_color","ring_number"
+           ,"ring_type","spore_print_color","habitat") #we remove this column in our dataset.
+train <- train[ , !(names(train) %in% drops)] #remove
+test <- test[ , !(names(test) %in% drops)] #remove
+#Warning: glm.fit: algorithm did not converge hatası iterasyon sayısı ile ilgili
+#default olarak maxit=25'dir biz 100 yapıyoruz
+model <- glm(class ~ ., data = train,family = binomial,maxit = 100)
+summary(model)
 
-#Assessing the predictive ability of the model
-fitted.results <- predict(model,newdata=subset(test),type='response')
-fitted.results <- ifelse(fitted.results > 0.5,"p","e")
-test_err <- mean(fitted.results != test$class)
+#şimdi modeli test datamız üzerinde test ediyoruz
+predicted <- predict.glm(model,newdata = test[,-1],type = "response")
+#tahmin edilen değerlerin 0.5 üzeri olanlar poison
+predicted <- predicted >= 0.5
+predicted <- gsub("TRUE","p",predicted)
+predicted <- gsub("FALSE","e",predicted)
 
-print(paste('Train : Accuracy',1-train_err,'Test : Accuracy',1-test_err))
-end_time <- Sys.time()
+actual <- test[,1]
 
-print(paste("running time => ", (end_time-start_time)))
+train_err <- mean(predicted != train$class)
+confusion_matrix <- table(predicted,actual)
+TP <- confusion_matrix[2,2]
+TN <- confusion_matrix[1,1]
+FP <- confusion_matrix[2,1]
+FN <- confusion_matrix[1,2]
+Accuracy <- (TP+TN)*100/(TP+TN+FP+FN)
+print(Accuracy)
+
+Recall <- TP*100/(TP+FN)
+print(Recall)
+
+F_Score <- 2*TP*100/(2*TP+FP+FN)
+print(F_Score)
+
+Precision <- TP*100/(TP+FP)
+print(Precision)
+
+#end_time <- Sys.time()
+
+#print(paste("running time => ", (end_time-start_time)))
 
 
 
 
 
-
+  
 
 # crossvalidation ? does not work
-train_control = trainControl(method="repeatedcv", number=5, repeats=5)
-modelLog = train(formula = class ~ ., data=train, method="glm", family=binomial, trControl=train_control)
-PredTrain = predict(modelLog, newdata=train, type="raw") 
-table(train$class, PredTrain > 0.5)
+#train_control = trainControl(method="repeatedcv", number=5, repeats=5)
+#modelLog = train(formula = class ~ ., data=train, method="glm", family=binomial, trControl=train_control)
+#PredTrain = predict(modelLog, newdata=train, type="raw") 
+#table(train$class, PredTrain > 0.5)
 
 
 
 #ROC Curve
 #install.packages("ROCR")
-library(ROCR)
-p <- predict(model, newdata=subset(test), type="response")
-pr <- prediction(p, test$class)
-prf <- performance(pr, measure = "tpr", x.measure = "fpr")
-plot(prf)
+#library(ROCR)
+#p <- predict(model, newdata=subset(test), type="response")
+#pr <- prediction(p, test$class)
+#prf <- performance(pr, measure = "tpr", x.measure = "fpr")
+#plot(prf)
 
-auc <- performance(pr, measure = "auc")
-auc <- auc@y.values[[1]]
-auc
+#auc <- performance(pr, measure = "auc")
+#auc <- auc@y.values[[1]]
+#auc
 #https://www.r-bloggers.com/how-to-perform-a-logistic-regression-in-r/
 
 
--- Preparation.R
+++ Preparation.R
@@ -1,37 +1,42 @@
-source(file = "LoadDataset.R") # load dataset
-#replace NA values to columns mode
-Mode <- function (x, na.rm) {
-  xtab <- table(x)
-  xmode <- names(which(xtab == max(xtab)))
-  if (length(xmode) > 1) xmode <- ">1 mode"
-  return(xmode)
-}
+mushroom <- read.csv("agaricus-lepiota.csv",header = TRUE) # load dataset
+#replace NA values to columns mode  
+#Mode <- function (x, na.rm) {
+#  xtab <- table(x)
+#  xmode <- names(which(xtab == max(xtab)))
+#  if (length(xmode) > 1) xmode <- ">1 mode"
+#  return(xmode)
+#}
 
-for (var in 1:ncol(mushroom)) {
-  mushroom[is.na(mushroom[,var]),var] <- Mode(mushroom[,var], na.rm = TRUE)
-}
+#for (var in 1:ncol(mushroom)) {
+#  mushroom[is.na(mushroom[,var]),var] <- Mode(mushroom[,var], na.rm = TRUE)
+#}
 
 #after data preparation of missing values.
 #missmap(mushroom, main = "After data preparation of missing values")
-
-#unique(mushroom$veil_type) # there is an unique value of veil_type column.
-drops <- c("veil_type") #there is one unique values of veil_type, we can remove this column in our dataset.
-mushroom <- mushroom[ , !(names(mushroom) %in% drops)] #remove veil_type
-#head(mushroom) # without numeric values, pure non preparing
-
-## categoric to numeric without target
-
-for (var in 2:ncol(mushroom)) {
-  mushroom[[var]] <- as.integer( mushroom[[var]])
+ComputeProportion <- function(target,attribute_dataset,Columns,centroid){
+  len_attr <- length(Columns)
+  RMSE <- NULL
+  for(i in 1:len_attr){
+    tab <- table(target,attribute_dataset[,Columns[i]])
+    prop <- tab[2,]/(tab[1,]+tab[2,])
+    centr_prop <- prop - centroid
+    Err <- sqrt(mean(centr_prop*centr_prop))
+    RMSE <-rbind(RMSE,Err)
+  }
+  norm_err <- (RMSE-min(RMSE))/(max(RMSE)-min(RMSE))
+  err_mat <- cbind(colnames(mushroom[,Columns]),RMSE)
+  err_mat <- cbind(err_mat,norm_err)
+  colnames(err_mat) <- c("Column name","RMSE","Norm. Error")
+  err_mat
 }
 
-#normalize integer values without class variable by robustHD library
-#mushroom[,2:22] <-  standardize(mushroom[,2:22])
+proportions_tab <- ComputeProportion(mushroom[,1],mushroom,c(2:23),0.482)
+print(proportions_tab)
 
-"for (var in 1:ncol(mushroom)) {
-  mushroom[[var]] <- as.factor( mushroom[[var]])
-}"
+#stalk_shape ve veil_type en düşük değere sahip
+drops <- c("veil_type","stalk_shape") #there is one unique values of veil_type, stalk_shape we can remove this column in our dataset.
+mushroom <- mushroom[ , !(names(mushroom) %in% drops)] #remove veil_type,stalk_shape
 
-#head(mushroom) # list the dataset after fill missing values, categoricial to numericial, and normalization.
+
 # remove unncessary data
-rm(columnNames,drops,var,Mode)
+rm(drops)
