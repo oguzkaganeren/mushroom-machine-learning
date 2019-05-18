f8302402b5a23c1649afdd14f265094970cb4347
Oguz Kagan EREN <oguz.eren@ceng.deu.edu.tr>
2019-05-18 19:59:37 +0300

    add importance level

--- Metrics.R
+++ Metrics.R
@@ -43,3 +43,40 @@ grid.arrange(myplots[[10]],myplots[[11]],myplots[[12]],myplots[[13]], ncol = 2)
 grid.arrange(myplots[[14]],myplots[[15]],myplots[[16]],myplots[[17]], ncol = 2)
 grid.arrange(myplots[[18]],myplots[[19]],myplots[[20]],myplots[[21]],myplots[[22]],myplots[[23]], ncol = 2)
 plot(mushroom$gill_attachment)
+
+
+#------------------------------Impotance level
+ComputeProportion <- function(target,attribute_dataset,Columns,centroid){
+  len_attr <- length(Columns)
+  RMSE <- NULL
+  for(i in 1:len_attr){
+    tab <- table(target,attribute_dataset[,Columns[i]])
+    prop <- tab[2,]/(tab[1,]+tab[2,])
+    centr_prop <- prop - centroid
+    Err <- sqrt(mean(centr_prop*centr_prop))
+    RMSE <-rbind(RMSE,Err)
+  }
+  norm_err <- (RMSE-min(RMSE))/(max(RMSE)-min(RMSE))
+  err_mat <- cbind(colnames(mushroom[,Columns]),RMSE)
+  err_mat <- cbind(err_mat,norm_err)
+  colnames(err_mat) <- c("Column name","RMSE","Norm. Error")
+  err_mat
+}
+
+#-------------------Importance Levels
+col<-c(2:23)
+secondCol<-c(1:23)
+secondCol<-secondCol[-4]
+proportions_tab <- ComputeProportion(mushroom[,1],mushroom,col,0.482)
+
+
+barplot(as.numeric(proportions_tab[,3]), ylab="Importance Level for edibility", 
+       names.arg=proportions_tab[,1],
+        border="blue", col=rainbow(3),las=2)
+#for second target
+proportions_tab <- ComputeProportion(mushroom[,5],mushroom,secondCol,0.415)
+print(proportions_tab)
+barplot(as.numeric(proportions_tab[,3]), ylab="Importance Level for bruises", 
+        names.arg=proportions_tab[,1],
+        border="blue", col=rainbow(3),las=2)
+
